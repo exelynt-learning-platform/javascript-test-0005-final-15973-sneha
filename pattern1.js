e1051e9c758cf0c2a5a74bff71537bda0207dcbb
Sneha Uttam Pangare <154306103+snehapangare@users.noreply.github.com>
2026-03-14 12:50:51 +0530

    Add pattern1.js to print alternating 1s and 0s

---
+++ pattern1.js
@@ -0,0 +1,11 @@
+for (let i = 1; i <= 6; i++) {   
+    let row = "";
+    for (let j = 1; j <= i; j++) {   
+        if ((i + j) % 2 === 0) {
+            row += "1 ";
+        } else {
+            row += "0 ";
+        }
+    }
+    console.log(row);
+}
